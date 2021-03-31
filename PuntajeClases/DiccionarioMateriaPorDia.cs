4976a347bc7d51fdaa803fe29fc1f8f576f90dfc
MatiasKala <matiraffi@gmail.com>
2021-03-31 04:59:36 -0300

    Se creo la clase DiccionarioMateriaPorDia que devuelve la categoria de la materia correspondiente al dia de la semana actual

---
+++ PuntajeClases/DiccionarioMateriaPorDia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuntajeClases.Unused
+{
+    class DiccionarioMateriaPorDia
+    {
+        public string CategoriaPorDia()
+        {
+            string retorno=null;
+
+            DayOfWeek dia = DateTime.Today.DayOfWeek;
+            if (dia.Equals(DayOfWeek.Monday))
+            {
+                retorno = CasoLunes();
+            }
+            Dictionary<DayOfWeek, string> dic = new Dictionary<DayOfWeek, string> ();
+          
+            return retorno;
+        }
+
+        private string CasoLunes()
+        {
+            string retorno = null;
+
+            if (DateTime.Now.Hour >= 23)
+            {
+                retorno = "BD2";
+            }
+            else
+            {
+                retorno = "AMS";
+            }
+
+            return retorno;
+        }
+    }
+}
