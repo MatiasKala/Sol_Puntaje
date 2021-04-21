cddd98e09ecde16c61d82a41c1fcfe770bce7709
MatiasKala <matiraffi@gmail.com>
2021-04-21 03:16:55 -0300

    Agregado nuevo documento Ideas en donde voy poniendo las ideas que voy teniendo para mejorar el programa y agregar nuevas funcionalidades

---
+++ PuntajeClases/Ideas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PuntajeClases
+{
+    class Class1
+    {
+        /*
+        Idea 1
+        Se podrian mejorar las sugerencias haciendo que el dia que tome̵no este harcodeado aca
+̵        sino que venga del dia que se ingreso anteriormente en la clase. para eso tendria que poder
+̵        transformar el string dd/mm/yy en un datetime y poder utilizarlo para ver que weekday es
+
+        HECHO :P
+        
+        Idea 2
+        Podriamos mejorar las sugerencias al fijarnos en la base de datos si la categoria que estamos
+        por sugerir ya fue cargada este mismo dia, entonces elegir otra, aunque lo dejo para otro dia
+
+        EN CURSO :s
+        
+        Idea 3 
+        Podriamos cambiar el diccionario de promedios y en vez de hacerlo double-string hacerlo
+        double-List<string> para evitar los problemas con los valores repetidos
+        EN CURSO :s
+
+        */
+    }
+}
